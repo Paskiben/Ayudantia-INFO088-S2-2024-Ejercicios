160a01fd3fe0692f994271aea466bcd01b358783
Paskiben <pascasalinas12@gmail.com>
2024-08-25 19:26:28 -0400

    Resultados semana 3 + semana 4

---
+++ Semana_4/ejercicio3.cpp
@@ -0,0 +1,14 @@
+/*
+    Ejecute el programa dandole dos palabras como argumento
+    Ejemplo: ./prog "palabra 1" "palabra 2"
+
+    Luego codifique la funcion bool is_bigger_than_word(char *palabra1, char* palabra2)
+    que retorne si la palabra1 > palabra2 en orden lexicografico.
+    Consideren que en caso de ser iguales si la palabra1 tiene mas caracteres que palabra2 entonces retorna true, false si no.
+*/
+
+bool is_bigger_than_word(char *palabra1, char* palabra2);
+
+int main(int argc, char** argv) {
+    
+}
